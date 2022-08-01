212495cc09915ed5330e10306ccb6692ea93fcba
myrzaerkinov <erkinovmyrza@gmail.com>
2022-08-01 15:42:55 +0600

    4rd

--- MensClothingStore/settings.py
+++ MensClothingStore/settings.py
@@ -88,12 +88,14 @@ WSGI_APPLICATION = "MensClothingStore.wsgi.application"
 
 DATABASES = {
     "default": {
-        'ENGINE': "django.db.backends.postgresql_psycopg2",
-        'NAME': os.environ.get('DB_NAME'),
-        'PORT': os.environ.get('DB_PORT'),
-        'HOST': os.environ.get('DB_HOST'),
-        'USER': os.environ.get('DB_USER'),
-        'PASSWORD': os.environ.get('DB_PASSWORD')
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'ENGINE': "django.db.backends.postgresql_psycopg2",
+        # 'NAME': os.environ.get('DB_NAME'),
+        # 'PORT': os.environ.get('DB_PORT'),
+        # 'HOST': os.environ.get('DB_HOST'),
+        # 'USER': os.environ.get('DB_USER'),
+        # 'PASSWORD': os.environ.get('DB_PASSWORD')
     }
 }
 
